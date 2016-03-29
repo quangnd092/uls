8ea4523de7178709bb79df2dd8b25caab5c73109
jsampang <jovensampang@jovens-mbp.intranet.ulsinc.com>
2016-03-29 10:59:10 -0700

    updates

--- scripts/main.8760756f.js
+++ scripts/main.8760756f.js
@@ -81,4 +81,15 @@ $(document).ready(function () {
             filter: d
         })
     })
+
 });
+
+function changeIt(imageName, objName) {
+    var obj = document.getElementById(objName);
+
+    var imgTag = "<img src='" + imageName + "' border='0' />";
+
+    obj.innerHTML = imgTag;
+
+    return;
+}
