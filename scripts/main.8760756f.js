29f6b5768b08a236293d8bdac57d6d9638e4f11c
jsampang <jovensampang@jovens-mbp.intranet.ulsinc.com>
2016-03-31 09:42:49 -0700

    updates

--- scripts/main.8760756f.js
+++ scripts/main.8760756f.js
@@ -84,6 +84,7 @@ $(document).ready(function () {
 
 });
 
+//  change image on nav tab
 function changeIt(imageName, objName) {
     var obj = document.getElementById(objName);
 
@@ -93,3 +94,23 @@ function changeIt(imageName, objName) {
 
     return;
 }
+
+//  back to top button
+$('body').prepend('<a href="#" class="back-to-top">Back to Top</a>');
+
+var amountScrolled = 300;
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > amountScrolled) {
+        $('a.back-to-top').fadeIn('slow');
+    } else {
+        $('a.back-to-top').fadeOut('slow');
+    }
+});
+
+$('a.back-to-top, a.simple-back-to-top').click(function () {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 700);
+    return false;
+});
