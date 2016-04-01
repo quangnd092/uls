6f72ade3e51b0f37d411c712f6c2b68ec510577b
jsampang <jovensampang@jovens-mbp.intranet.ulsinc.com>
2016-04-01 16:10:17 -0700

    update

--- search/search_content.js
+++ search/search_content.js
@@ -1,6 +1,6 @@
 var tipuedrop = {"pages": [
      {"title": "<b>Wood</b> Engraving and Cutting with Laser...", "thumb": "search/img/wood01.png", "text": "a site search engine jQuery plugin", "url": "search-result.html"},
-     {"title": "<b>Wood</b>working Custom woodworking with Universal Laser...", "thumb": "search/img/wood02.png", "text": "a search suggestion box jQuery plugin", "url": "search-result.html"},
+     {"title": "<b>Wood</b>working Custom <b>wood</b>working with Universal Laser...", "thumb": "search/img/wood02.png", "text": "a search suggestion box jQuery plugin", "url": "search-result.html"},
      {"title": "Most <b>wood</b> types are easy to cut using DLMP® technology.", "thumb": "search/img/wood03.png", "text": "a small and simple tooltip jQuery plugin", "url": "search-result.html"},
       {"title": "View All", "thumb": "search/img/search-all.png", "text": "a site search engine jQuery plugin", "url": "search-result.html"}
 ]};
