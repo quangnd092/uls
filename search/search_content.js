af6ce6e8030c40af0872ab89a1d8d9b240252fd9
jsampang <jovensampang@jovens-mbp.intranet.ulsinc.com>
2016-04-01 15:33:29 -0700

    Updates

---
+++ search/search_content.js
@@ -0,0 +1,7 @@
+var tipuedrop = {"pages": [
+     {"title": "Wood Engraving and Cutting with Laser...", "thumb": "search/img/wood01.png", "text": "a site search engine jQuery plugin", "url": "search-result.html"},
+     {"title": "Woodworking Custom woodworking with Universal Laser...", "thumb": "search/img/wood02.png", "text": "a search suggestion box jQuery plugin", "url": "search-result.html"},
+     {"title": "Most wood types are easy to cut using DLMP® technology.", "thumb": "search/img/wood03.png", "text": "a small and simple tooltip jQuery plugin", "url": "search-result.html"},
+      {"title": "View All", "thumb": "search/img/search-all.png", "text": "a site search engine jQuery plugin", "url": "search-result.html"}
+]};
+
